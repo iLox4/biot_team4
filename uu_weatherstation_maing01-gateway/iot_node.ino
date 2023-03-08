bafee3ca8b05f0284e110fc8dbd57a9b26a99501
Yurii Bliusiuk <ura.blusuk@gmail.com>
2023-03-08 18:55:38 +0100

    IoT Node sketch and NodeRed flows added

---
+++ uu_weatherstation_maing01-gateway/iot_node.ino
@@ -0,0 +1,32 @@
+#include <dht.h>
+#define dataPin 8 // Defines pin number to which the sensor is connected
+dht DHT;
+
+unsigned long previousMillis = 0;
+long interval = 2000;
+
+void setup() 
+{
+	Serial.begin(9600);
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    int readData = DHT.read22(dataPin);
+
+    float t = DHT.temperature; // Gets the values of the temperature
+    float h = DHT.humidity; // Gets the values of the humidity
+
+    // Printing the results on the serial monitor
+    Serial.print("{\"temperature\":");
+    Serial.print(t);
+    Serial.print(",\"humidity\":");
+    Serial.print(h);
+    Serial.println("}");
+  }
+}
