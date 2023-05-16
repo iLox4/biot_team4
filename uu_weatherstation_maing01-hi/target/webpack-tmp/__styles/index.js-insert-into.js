de84195eeb9139bc7ed4cb4bc893633f41b8e665
Yurii Bliusiuk <ura.blusuk@gmail.com>
2023-05-16 17:03:06 +0200

    deploy

--- uu_weatherstation_maing01-hi/target/webpack-tmp/__styles/index.js-insert-into.js
+++ uu_weatherstation_maing01-hi/target/webpack-tmp/__styles/index.js-insert-into.js
@@ -1,7 +1,7 @@
 function insert(styleEl) {
   let insertionEl = document.head || document.body;
 
-  styleEl.setAttribute("data-owner", "uu5_tutorial_maing01-hi/uu5_tutorial_maing01-hi@0.1.0");
+  styleEl.setAttribute("data-owner", "uu5_tutorial_maing01-hi/uu5_tutorial_maing01-hi@0.1.2");
   styleEl.setAttribute("data-emotion", "uu-tutorial");
   styleEl.setAttribute("data-tech", "less");
 
--- uu_weatherstation_maing01-hi/target/webpack-tmp/index-umd.js
+++ uu_weatherstation_maing01-hi/target/webpack-tmp/index-umd.js
@@ -3,7 +3,7 @@ var isDoc = typeof document !== "undefined";
 var uri = ((mod ? mod.uri : isDoc && (document.currentScript || Array.prototype.slice.call(document.getElementsByTagName("script"), -1)[0] || {}).src) || "").toString();
 uri = uri.split(/\//).slice(0, -1).join("/") + "/"; // runtime publicPath configuration required for proper linking of styles, background images, ...
 var floatingVersion = "/0.0.0/";
-if (uri.substr(-floatingVersion.length) === floatingVersion) uri = uri.substr(0, uri.length - floatingVersion.length) + "/0.1.0/";
+if (uri.substr(-floatingVersion.length) === floatingVersion) uri = uri.substr(0, uri.length - floatingVersion.length) + "/0.1.2/";
 __webpack_public_path__ = uri;
 module.exports = require("__project__/index.js");
 var ex = module.exports;
